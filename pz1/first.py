6fb07dd8faf5049b645f82241a65e04ba39ed1e5
Kostiantyn <sinkkostia@gmail.com>
2025-02-10 22:13:32 +0200

    Fist task completed

---
+++ pz1/first.py
@@ -0,0 +1,20 @@
+class InstanceCounter:
+    _count = 0
+
+    def __init__(self):
+        type(self)._count += 1
+
+    @classmethod
+    def get_count(cls):
+        return cls._count
+
+    def __del__(self):
+        type(self)._count -= 1
+
+obj1 = InstanceCounter()
+obj2 = InstanceCounter()
+obj3 = InstanceCounter()
+print(InstanceCounter.get_count())
+
+del obj1
+print(InstanceCounter.get_count())
